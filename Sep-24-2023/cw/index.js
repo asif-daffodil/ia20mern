b7efd531e746145f5c4496d0651b6c22700e3af9
asif-daffodil <abir@dipti.com.bd>
2023-09-24 12:28:50 +0600

    sep-24-2023

---
+++ Sep-24-2023/cw/index.js
@@ -0,0 +1,38 @@
+console.log('Hello World');
+
+// signle line comment
+/*
+multi
+line 
+comment
+*/
+
+// variable
+
+// var / let / const
+
+// reassign / redeclare
+
+let student_Name = 'Krishna Kumar';
+console.log(student_Name);
+
+student_Name = 'Raju Bepari';
+console.log(student_Name);
+
+// const
+const student_Name1 = 'Maruf bin Salim';
+// student_Name1 = "Bappy Bepari";
+console.log(student_Name1);
+
+// data types
+/*
+    1. primitive data types
+        a. string
+        b. number
+        c. boolean
+        d. undefined
+        e. null
+        f. symbol
+    2. object
+    3. function
+*/
